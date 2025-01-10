144a203f3265b387683b40f174d494fb56f77e25
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2025-01-10 16:15:24 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const port = process.env.HOST_PORT;
+const domain = process.env.HOST_DOMAIN;
+const app = express();
+
+//SERVER LISTENING
+app.listen(port, () => {
+  console.log(`Server listening at ${domain}:${port}`);
+});
