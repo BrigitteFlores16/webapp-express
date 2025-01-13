bbec0336b8075046d8739e8bd212b0662c4f196f
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2025-01-13 19:02:32 +0100

    aggiunto CORS

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { APP_HOST, APP_PORT } = process.env;
 const connection = require("./configurazione.js");
 const app = express();
 
+var corsOptions = {
+  origin: "http://localhost:5173",
+  OptionsSuccessStatus: 200,
+};
+
 const moviesRouter = require("./routers/moviesRouter");
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/movies", moviesRouter);
